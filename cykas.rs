5d7c2a1ce7add118e94917a041eba13891fadb27
Jeremy Ruten <jeremy.ruten@gmail.com>
2014-07-18 01:35:35 -0600

    add ripemd160 hashing in addition to sha256.

--- cykas.rs
+++ cykas.rs
@@ -3,7 +3,7 @@ extern crate libc;
 mod crypto;
 
 fn main() {
-    let hash = crypto::sha256([65]);
+    let hash = crypto::hash::sha256([65]);
     for byte in hash.iter() {
         print!("{:02x}", *byte);
     }
