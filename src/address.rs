ed80d614d0650759a847e1fd68ab861b27c3eb1c
Jeremy Ruten <jeremy.ruten@gmail.com>
2014-08-06 23:33:33 -0600

    Reorganize stuff, split private_key.rs off of address.rs.

--- src/address.rs
+++ src/address.rs
@@ -1,110 +1,49 @@
-use openssl::crypto::rand::rand_bytes;
-use openssl::crypto::hash::{hash, SHA256, RIPEMD160};
-use ecdsa::derive_public_key;
-use base58::base58_encode;
+use private_key;
 
-// A Bitcoin private key is 32 bytes in length.
-static PRIVATE_KEY_LENGTH: uint = 32u;
-
-// Bitcoin keys must be less than or equal to this value, as dictated by the
-// secp256k1 curve it uses.
-static MAX_PRIVATE_KEY: &'static [u8] = &[
-    0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
-    0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xfe,
-    0xba,0xae,0xdc,0xe6,0xaf,0x48,0xa0,0x3b,
-    0xbf,0xd2,0x5e,0x8c,0xd0,0x36,0x41,0x40
-];
-
-// ...also, Bitcoin keys can't be zero.
-static ZERO_PRIVATE_KEY: &'static [u8] = &[
-    0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-    0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-    0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-    0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-];
-
-// A Bitcoin address. Represented as just a private key for now (which can be
-// used to derive the public key and public address).
+// A Bitcoin address. Represented as a standard base-58 public address, and an
+// optional private key.
 pub struct Address {
-    private_key: Vec<u8>
+    public_address: String,
+    private_key: Option<Vec<u8>>
 }
 
 impl Address {
-    // Create new Address from 256-bit private key.
-    pub fn new(private_key: &[u8]) -> Address {
-        if !Address::is_private_key_valid(private_key) {
+    // Creates a new Address from a standard base-58 representation of a
+    // Bitcoin address.
+    pub fn from_base58(address: &str) -> Address {
+        // TODO: validate it.
+        Address {
+            public_address: address.to_string(),
+            private_key: None
+        }
+    }
+
+    // Creates a new Address from a 256-bit private key.
+    pub fn from_private_key(key: &[u8]) -> Address {
+        if !private_key::is_valid(key) {
             fail!("Invalid private key!");
         }
 
-        Address { private_key: Vec::from_slice(private_key) }
+        Address {
+            public_address: private_key::derive_public_address(key),
+            private_key: Some(Vec::from_slice(key))
+        }
     }
 
     // Generate a new Address using openssl's random bytes generator.
     pub fn new_random() -> Address {
-        let mut key;
-        loop {
-            // Generate a random key by asking openssl for 32 random bytes.
-            // Keep generating a new one until we get a valid one. (The range
-            // of invalid private keys is so tiny that it should pretty much
-            // never happen.)
-            key = rand_bytes(PRIVATE_KEY_LENGTH);
-            if Address::is_private_key_valid(key.as_slice()) { break; }
-        }
-        Address { private_key: key }
+        let key = private_key::generate();
+        Address::from_private_key(key.as_slice())
     }
 
-    // Check if the given private key is valid. A Bitcoin private key is valid
-    // if it is exactly 32 bytes long, and is less than MAX_PRIVATE_KEY, and
-    // isn't zero.
-    fn is_private_key_valid(private_key: &[u8]) -> bool {
-        private_key.len() == PRIVATE_KEY_LENGTH &&
-        private_key > ZERO_PRIVATE_KEY &&
-        private_key <= MAX_PRIVATE_KEY
-    }
-
-    // Get the public address as a String.
-    pub fn to_string(&self) -> String {
-        // Converting a private key to a Bitcoin address is a process with many
-        // steps. First we derive the public key from the private key.
-        let pub_key = derive_public_key(self.private_key.as_slice());
-
-        // Next, obtain the SHA256 hash of the public key.
-        let pub_key_sha = hash(SHA256, pub_key.as_slice());
-
-        // Now obtain the RIPEMD-160 hash of that.
-        let mut pub_key_ripemd = hash(RIPEMD160, pub_key_sha.as_slice());
-
-        // Stick a version byte (0x00) to the front of that hash, to identify
-        // this as a Bitcoin address. (It's why Bitcoin addresses start with a
-        // "1".)
-        pub_key_ripemd.insert(0, 0x00);
-
-        // Perform a double SHA256 hash of the above hash that has the version
-        // byte in front.
-        let mut double_sha = hash(SHA256, pub_key_ripemd.as_slice());
-        double_sha = hash(SHA256, double_sha.as_slice());
-
-        // Get the first 4 bytes of the double hash. This will be the address's
-        // checksum.
-        let checksum = double_sha.slice(0, 4);
-
-        // Stick the checksum onto the end of the RIPEMD-160 hash that has the
-        // version byte in front.
-        pub_key_ripemd.push_all(checksum);
-
-        for b in pub_key_ripemd.iter() { print!("{:02x}", *b); } println!("");
-
-        // pub_key_ripemd now consists of a version byte (0x00), a RIPEMD-160
-        // hash, and a 4-byte checksum all concatenated together. The last step
-        // is to convert this string of bytes to a base58 string.
-        base58_encode(pub_key_ripemd.as_slice())
+    pub fn get_public_address(&self) -> &str {
+        self.public_address.as_slice()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::Address;
-    use super::{PRIVATE_KEY_LENGTH, ZERO_PRIVATE_KEY, MAX_PRIVATE_KEY};
 
     static INVALID_PRIVATE_KEY: &'static [u8] =
         &[0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
@@ -115,45 +54,27 @@ mod tests {
     static VALID_PRIVATE_KEY_ADDRESS: &'static str = "19gL5Rq1uc5yspAtbM7NyDs1godKnGHMar";
 
     #[test]
-    fn test_new() {
-        Address::new(VALID_PRIVATE_KEY);
+    fn test_from_private_key() {
+        let address = Address::from_private_key(VALID_PRIVATE_KEY);
+        assert_eq!(address.private_key.unwrap().as_slice(), VALID_PRIVATE_KEY);
+        assert_eq!(address.public_address.as_slice(), VALID_PRIVATE_KEY_ADDRESS);
     }
 
     #[test]
     #[should_fail]
-    fn test_new_with_invalid_key() {
-        Address::new(INVALID_PRIVATE_KEY);
-    }
-
-    #[test]
-    #[should_fail]
-    fn test_new_with_invalid_zero_key() {
-        Address::new(ZERO_PRIVATE_KEY);
+    fn test_from_private_key_with_invalid_key() {
+        Address::from_private_key(INVALID_PRIVATE_KEY);
     }
 
     #[test]
     fn test_new_random() {
         let address = Address::new_random();
-        assert!(address.private_key.len() == PRIVATE_KEY_LENGTH);
+        assert!(address.private_key != None);
 
         // If the same address is generated again, then there's a serious
         // problem. Even if it can happen in theory.
-        let address2 = Address::new_random();
-        assert!(address.private_key != address2.private_key);
-    }
-
-    #[test]
-    fn test_is_private_key_valid() {
-        assert!(!Address::is_private_key_valid(INVALID_PRIVATE_KEY));
-        assert!(!Address::is_private_key_valid(ZERO_PRIVATE_KEY));
-        assert!(Address::is_private_key_valid(VALID_PRIVATE_KEY));
-        assert!(Address::is_private_key_valid(MAX_PRIVATE_KEY));
-    }
-
-    #[test]
-    fn test_to_string() {
-        let address = Address::new(VALID_PRIVATE_KEY);
-        assert_eq!(address.to_string().as_slice(), VALID_PRIVATE_KEY_ADDRESS);
+        let another = Address::new_random();
+        assert!(address.private_key != another.private_key);
     }
 }
 
--- src/base58.rs
+++ src/base58.rs
@@ -2,10 +2,10 @@ use std::num::Zero;
 use num::bigint::{BigUint,ToBigUint};
 use num::Integer;
 
-static BASE58_ALPHABET: &'static str = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+static ALPHABET: &'static str = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
-// Converts `data` to a base-58 string, that can be decoded by base58_decode().
-pub fn base58_encode(data: &[u8]) -> String {
+// Converts `data` to a base-58 string, that can be decoded by decode().
+pub fn encode(data: &[u8]) -> String {
     // Count the number of zero bytes at the beginning of the data. In the
     // Bitcoin base-58 format, leading zero bytes are treated separately. After
     // the real base-58 conversion, they will be added back as '1' characters.
@@ -36,7 +36,7 @@ pub fn base58_encode(data: &[u8]) -> String {
         // have one of 58 values, and use it as the next digit in the result
         // string. The rest of the number becomes the new `n`.
         let (rest_of_n, digit) = n.div_rem(&fifty_eight);
-        result.push_char(BASE58_ALPHABET.char_at(digit.to_uint().unwrap()));
+        result.push_char(ALPHABET.char_at(digit.to_uint().unwrap()));
         n = rest_of_n;
     }
 
@@ -52,7 +52,7 @@ pub fn base58_encode(data: &[u8]) -> String {
 
 // Converts a base-58 string back to the data it represents, as a vector of
 // bytes.
-pub fn base58_decode(string: &str) -> Vec<u8> {
+pub fn decode(string: &str) -> Vec<u8> {
     // Count the number of leading zeroes at the beginning of the string. Each
     // leading zero (which appears as '1' in base-58) represents one zero-byte
     // at the beginning of the decoded data, so we treat these separately
@@ -75,7 +75,7 @@ pub fn base58_decode(string: &str) -> Vec<u8> {
     let mut multiplier: BigUint = 1u.to_biguint().unwrap();
     let fifty_eight = 58u.to_biguint().unwrap();
     for digit in string.chars().rev() {
-        let digit = BASE58_ALPHABET.chars().position(|ch| ch == digit ).unwrap();
+        let digit = ALPHABET.chars().position(|ch| ch == digit ).unwrap();
         n = n + digit.to_biguint().unwrap() * multiplier;
         multiplier = multiplier * fifty_eight;
     }
@@ -103,64 +103,64 @@ pub fn base58_decode(string: &str) -> Vec<u8> {
 
 #[cfg(test)]
 mod tests {
-    use super::{base58_encode,base58_decode};
+    use super::{encode,decode};
 
     #[test]
-    fn test_base58_encode_bitcoin_addresses() {
+    fn test_encode_bitcoin_addresses() {
         // Normal Bitcoin address.
         let data = [0x00,0x78,0x97,0x0e,0x37,0xa7,0xa4,0x71,0xdc,0x33,0xda,0xdb,
                     0x51,0x42,0x06,0x84,0x31,0xb4,0x85,0x17,0xab,0x00,0xed,0xb1,0x45];
-        assert_eq!(base58_encode(data).as_slice(), "1Bzd3YTSDwdFfAhMwYNV6A3K5hwYHbaUeG");
+        assert_eq!(encode(data).as_slice(), "1Bzd3YTSDwdFfAhMwYNV6A3K5hwYHbaUeG");
 
         // Bitcoin address that starts with two 1's.
         let data = [0x00,0x00,0x51,0x34,0xda,0xfd,0x60,0x2d,0xcc,0x55,0x36,0x87,
                     0x06,0xd7,0x56,0xc7,0x4f,0xb7,0x74,0x48,0x31,0xf3,0x22,0xdc,0xe3];
-        assert_eq!(base58_encode(data).as_slice(), "112gHKoeKQ3PEXEdAZeC5tBoonPR2UCQot");
+        assert_eq!(encode(data).as_slice(), "112gHKoeKQ3PEXEdAZeC5tBoonPR2UCQot");
 
         // Bitcoin address that starts with three 1's (took a while to find
         // this one).
         let data = [0x00,0x00,0x00,0x23,0xc5,0x36,0xed,0x86,0x7d,0x66,0xa0,0x6b,
                     0x5a,0xfe,0x67,0x5d,0xe8,0xcb,0xe9,0x03,0x94,0x33,0xe5,0x57,0x3d];
-        assert_eq!(base58_encode(data).as_slice(), "111Ai6JPjhcuWxu6ULnRtk34cEj2ZJXfa");
+        assert_eq!(encode(data).as_slice(), "111Ai6JPjhcuWxu6ULnRtk34cEj2ZJXfa");
     }
 
     #[test]
-    fn test_base58_encode_out_of_bounds() {
+    fn test_encode_out_of_bounds() {
         // This test case caused an out of bounds bug at one point.
         let data = [0x00,0xc3,0xd2,0x6e,0xc2,0x04,0x8a,0x9f,0x52,0x8f,0xfb,0xbf,
                     0xf4,0xb8,0x08,0xaf,0x16,0x7e,0x12,0xac,0x82,0x5b,0x40,0x2b,0xb2];
-        base58_encode(data);
+        encode(data);
     }
 
     #[test]
-    fn test_base58_encode_zeroes() {
+    fn test_encode_zeroes() {
         let data = [0x00,0x00,0x00,0x00];
-        assert_eq!(base58_encode(data).as_slice(), "1111");
+        assert_eq!(encode(data).as_slice(), "1111");
     }
 
     #[test]
-    fn test_base58_decode_bitcoin_addresses() {
+    fn test_decode_bitcoin_addresses() {
         // Normal Bitcoin address.
         let data: &[u8] = &[0x00u8,0x78,0x97,0x0e,0x37,0xa7,0xa4,0x71,0xdc,0x33,0xda,0xdb,
                             0x51,0x42,0x06,0x84,0x31,0xb4,0x85,0x17,0xab,0x00,0xed,0xb1,0x45];
-        assert_eq!(base58_decode("1Bzd3YTSDwdFfAhMwYNV6A3K5hwYHbaUeG").as_slice(), data);
+        assert_eq!(decode("1Bzd3YTSDwdFfAhMwYNV6A3K5hwYHbaUeG").as_slice(), data);
 
         // Bitcoin address that starts with two 1's.
         let data: &[u8] = &[0x00,0x00,0x51,0x34,0xda,0xfd,0x60,0x2d,0xcc,0x55,0x36,0x87,
                             0x06,0xd7,0x56,0xc7,0x4f,0xb7,0x74,0x48,0x31,0xf3,0x22,0xdc,0xe3];
-        assert_eq!(base58_decode("112gHKoeKQ3PEXEdAZeC5tBoonPR2UCQot").as_slice(), data);
+        assert_eq!(decode("112gHKoeKQ3PEXEdAZeC5tBoonPR2UCQot").as_slice(), data);
 
         // Bitcoin address that starts with three 1's (took a while to find
         // this one).
         let data: &[u8] = &[0x00,0x00,0x00,0x23,0xc5,0x36,0xed,0x86,0x7d,0x66,0xa0,0x6b,
                             0x5a,0xfe,0x67,0x5d,0xe8,0xcb,0xe9,0x03,0x94,0x33,0xe5,0x57,0x3d];
-        assert_eq!(base58_decode("111Ai6JPjhcuWxu6ULnRtk34cEj2ZJXfa").as_slice(), data);
+        assert_eq!(decode("111Ai6JPjhcuWxu6ULnRtk34cEj2ZJXfa").as_slice(), data);
     }
 
     #[test]
-    fn test_base58_decode_zeroes() {
+    fn test_decode_zeroes() {
         let data: &[u8] = &[0x00,0x00,0x00,0x00];
-        assert_eq!(base58_decode("1111").as_slice(), data);
+        assert_eq!(decode("1111").as_slice(), data);
     }
 }
 
